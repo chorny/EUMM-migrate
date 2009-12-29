bd8af515f4fffc46eeaa659daec7e23dadd4fea3
Alexandr Ciornii <alexchorny@gmail.com>
2009-12-29 14:41:50 +0200

    version 0.02

--- lib/App/EUMM/Migrate.pm
+++ lib/App/EUMM/Migrate.pm
@@ -9,11 +9,11 @@ App::EUMM::Migrate - Perl tool to migrate from ExtUtils::MakeMaker to Module::Bu
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
